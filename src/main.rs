bd1b2399e96ec78d4463dce63c6f82dcb8bd55af
Jason Sweet <hoozifachi@gmail.com>
2025-08-04 21:53:01 -0500

    Changes background color.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("MyGame")]
 async fn main() {
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(DARKGRAY);
 
         next_frame().await
     }
