5b21e799431aab04444181960025c13a6916d5ef
Jason Sweet <hoozifachi@gmail.com>
2025-08-05 08:20:42 -0500

    Adds moving circle.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,28 @@ use macroquad::prelude::*;
 
 #[macroquad::main("MyGame")]
 async fn main() {
+    const SPEED: f32 = 5.0;
+
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
+
     loop {
         clear_background(DARKPURPLE);
+
+        if is_key_down(KeyCode::Right) {
+            x += SPEED;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= SPEED;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += SPEED;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= SPEED;
+        }
+
+        draw_circle(x, y, 16.0, YELLOW);
 
         next_frame().await
     }
