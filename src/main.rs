9124a96bfa7ddf717fd1c87c46fa02f162a6ef63
Jason Sweet <hoozifachi@gmail.com>
2025-08-05 08:30:52 -0500

    Bases movement on time,  Clamps ball to the screen.

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,34 @@ use macroquad::prelude::*;
 
 #[macroquad::main("MyGame")]
 async fn main() {
-    const SPEED: f32 = 5.0;
+    const MOVEMENT_SPEED: f32 = 200.0;
+    const RADIUS: f32 = 16.0;
 
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
 
     loop {
+        let delta_time = get_frame_time();
+
         clear_background(DARKPURPLE);
 
         if is_key_down(KeyCode::Right) {
-            x += SPEED;
+            x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= SPEED;
+            x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += SPEED;
+            y += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= SPEED;
+            y -= MOVEMENT_SPEED * delta_time;
         }
 
-        draw_circle(x, y, 16.0, YELLOW);
+        draw_circle(x, y, RADIUS, YELLOW);
+
+        x = clamp(x, 0.0 + RADIUS, screen_width() - RADIUS);
+        y = clamp(y, 0.0 + RADIUS, screen_height() - RADIUS);
 
         next_frame().await
     }
