6895872821f6106479bbbc924a007a998c4ea6e5
Jason Sweet <hoozifachi@gmail.com>
2025-08-09 10:28:29 -0500

    Adds texture to explosion.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use macroquad::experimental::animation::{AnimatedSprite, Animation};
 use macroquad::prelude::*;
-use macroquad_particles::{self as particles, ColorCurve, Emitter, EmitterConfig};
+use macroquad_particles::{self as particles, AtlasConfig, ColorCurve, Emitter, EmitterConfig};
 use std::fs;
 
 #[cfg(not(target_arch = "wasm32"))]
@@ -63,14 +63,10 @@ fn particle_explosion() -> particles::EmitterConfig {
         lifetime_randomness: 0.3,
         explosiveness: 0.65,
         initial_direction_spread: 2.0 * std::f32::consts::PI,
-        initial_velocity: 300.0,
+        initial_velocity: 400.0,
         initial_velocity_randomness: 0.8,
-        size: 3.0,
-        colors_curve: ColorCurve {
-            start: RED,
-            mid: ORANGE,
-            end: RED,
-        },
+        size: 16.0,
+        atlas: Some(AtlasConfig::new(5, 1, 0..)),
         ..Default::default()
     }
 }
@@ -131,6 +127,10 @@ async fn main() {
         .await
         .expect("Couldn't load laser-bolt.png");
     bullet_texture.set_filter(FilterMode::Nearest);
+    let explosion_texture = load_texture("explosion.png")
+        .await
+        .expect("Couldn't load explosion.png");
+    explosion_texture.set_filter(FilterMode::Nearest);
     build_textures_atlas();
 
     let mut bullet_sprite = AnimatedSprite::new(
@@ -358,7 +358,8 @@ async fn main() {
                             high_score = high_score.max(score);
                             explosions.push((
                                 Emitter::new(EmitterConfig {
-                                    amount: square.size.round() as u32 * 2,
+                                    amount: square.size.round() as u32 * 4,
+                                    texture: Some(explosion_texture.clone()),
                                     ..particle_explosion()
                                 }),
                                 vec2(square.x, square.y),
